4311ffbaf1441fdcee45faa43b796bf93bf617cb
nurulamin092 <mnurulamin092@gmial.com>
2022-04-22 11:51:41 +0600

    mongodb connection.

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
-const express = requeir('express');
-const app= express();
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
 const port = process.env.PORT || 5000;
-app.get ('/',(req,res)=>{
-    res.send('Running my node crud  server')
-})
+
+//use middleware
+app.use(cors());
+app.use(express.json());
+
+//user:dbuser1
+//password:4JpnKigFYqMkC6oV
+
+const uri = "mongodb+srv://dbuser1:4JpnKigFYqMkC6oV@cluster0.qo3jo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    console.log('db connected');
+    // perform actions on the collection object
+    client.close();
+});
+
+
+app.get('/', (req, res) => {
+    res.send('Running MY NODE    CRUD  SERVER')
+});
+
+app.listen(port, () => {
+    console.log('crud server is running');
+}) 
